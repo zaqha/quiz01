139a895e260daf1dd7ce63786b2eb02ae9c00e4d
zaqha <zaqha12@gmail.com>
2021-06-24 21:48:44 +0800

    add explanation

--- base.php
+++ base.php
@@ -51,34 +51,48 @@ class DB{
     public function all(...$arg){
         $sql="select * from $this->table ";    
         
+        // $arg=[] or [陣列],[SQL字串],[陣列,SQL字串],
+
+
         //判斷第一個參數是否存在
         if(isset($arg[0])){
-            //if $arg[0]是陣列 
-            // ["欄位"=>"值","欄位"=>"值"]
-            // where `欄位`='值' && `欄位`='值'
-            //"欄位"=>"值" ===>`欄位`＝'值'
-            // %s =>字串
-            foreach($arg[0] as $key => $value){
-                $tmp[]=sprintf("`%s`='%s'",$key,$value);
+            if(is_array($arg[0])){
+                //if $arg[0]是陣列 
+                // ex 把 ['acc'='XXX'
+                //        'pw' = "1234"]
+                // 改成字串 where acc='XXX' && pw = "1234"
+
+                // ["欄位"=>"值","欄位"=>"值"]
+                // where `欄位`='值' && `欄位`='值'
+                //"欄位"=>"值" ===>`欄位`＝'值'
+                // %s =>字串 (正規表達式)
+                foreach($arg[0] as $key => $value){
+                    $tmp[]=sprintf("`%s`='%s'",$key,$value);
+                    // (Before)[0]=[[name]=>amy, [visible]=>Y]
+                    // (Result)[0]=>`name`='amy' [1]=>`visible`='Y'
+                }
+                //組合條件語句字串
+                //ex "where `visible`='Y'"
+
+                //(空)&&(空) 一定要空白
+                $sql=$sql . "where" . implode(" && ",$tmp);
+                // (Result) select * from $this->table where `name`='amy' && `visible`='Y'
+                
+            }else{
+                // else不是陣列/當它是字串
+                $sql=$sql . $arg[0];
             }
-            //組合條件語句字串
-            //(空)&&(空) 一定要空白
-            $sql=$sql . "where" . implode(" && ",$tmp);
-            
-        }else{
-            // else當它是字串
-            $sql=$sql . $arg[0];
-        }
 
-        // //如果有第二個元素，表示條件後有其他附帶的SQL語法，
-        //則我們規定第二個參數為一個sql的語法字串
-        //因此直接將語法字串接在$sql之後即可
-        if(isset($arg[1])){
-            // 當它是字串
-            $sql=$sql . $arg[1];
+            // //如果有第二個元素，表示條件後有其他附帶的SQL語法，
+            //則我們規定第二個參數為一個sql的語法字串
+            //因此直接將語法字串接在$sql之後即可
+            // ex acc="mack" or acc='admin' "where `visible`='Y'", order by...
+            if(isset($arg[1])){
+                // 當它是字串
+                $sql=$sql . $arg[1];
+            }
         }
-
-        //沒參數的時侯 
+        //空陣列/沒參數的時侯，$sql="select * from $this->table "
         return $this->pdo->query($sql)->fetchAll();
     }
 
@@ -103,6 +117,9 @@ class DB{
         return $this->pdo->query($sql)->fetchColumn();
     }
 
+    // $User=new DB("stories")
+    // print_r($User->count()(" where name ='amy' "));
+
     //取得單筆資料
     public function find($id){
         $sql="select * from $this->table ";
@@ -113,7 +130,9 @@ class DB{
                     $sql=$sql . " where " . implode(" && ",$tmp);
             }else{
                 $sql=$sql . " where `id`='$id'";
+                // "where `id`='".$id."'"; 可省略
             }
+            // query查詢
         return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
 
@@ -126,32 +145,34 @@ class DB{
                 }
                     $sql=$sql . " where " . implode(" && ",$tmp);
             }else{
-
                 $sql=$sql . " where `id`='$id'";
             }
+            //exec 執行 result=>幾筆被影響
         return $this->pdo->exec($sql);
-
     }
 
     //新增\更新資料
+    //INSERT INTO stores (id, name , location, price, people) VALUE (NULL, '$name', '$location', '$price', '$people')
+    //UPDATE `stores` SET `name`='$name',`location`='$location',`price`='$price',`people`='$people' WHERE id = '$id'
+    //差別在於有無 where id
     public function save($array){
+        // 省略 if(is_array)
         if(isset($array['id'])){
-            //update
+            //update 有ID就是要做更新
                 foreach($array as $key => $value){
                     if($key!='id'){
                         $tmp[]=sprintf("`%s`='%s'",$key,$value);
                     }
                 }
-
+                // 不能這樣寫 "update $this->table set ".implode(',',$tmp)." where `id`='$array['id']'"
             $sql="update $this->table set ".implode(',',$tmp)." where `id`='{$array['id']}'";
         }else{
-            //insert
+            //insert 沒ID是要做新增
             // `name`,`addr`,`tel`
             $sql="insert into $this->table 
                     (`".implode("`,`",array_keys($array))."`) values
                     ('".implode("','",$array)."')";
         }
-
         return $this->pdo->exec($sql);
     }
 
